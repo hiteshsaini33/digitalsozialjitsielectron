8674b65df009e2311a3e11c1fe1d2875d9b2f33c
hiteshsaini33 <sainihitesh33@gmail.com>
2021-04-10 12:45:34 +0530

    first commit

---
+++ app/features/config/index.js
@@ -0,0 +1,48 @@
+
+export default {
+    /**
+     * The URL with extra information about the app / service.
+     */
+    aboutURL: 'https://digitalesozialearbeit.de',
+
+    /**
+     * The URL to the source code repository.
+     */
+    sourceURL: 'https://digitalesozialearbeit.de',
+
+    /**
+     * Application name.
+     */
+    appName: 'Digitalesozialearbeit',
+
+    /**
+    * The prefix for application protocol.
+    * You will also need to replace this in package.json.
+    */
+    appProtocolPrefix: 'digitalesozialearbeit',
+
+    /**
+     * The default server URL of Jitsi Meet Deployment that will be used.
+     */
+    defaultServerURL: 'https://jitsi.digitalesozialearbeit.de',
+
+    /**
+     * The default server Timeout in seconds.
+     */
+    defaultServerTimeout: 30,
+
+    /**
+     * URL to send feedback.
+     */
+    feedbackURL: 'https://digitalesozialearbeit.de/kontakt',
+
+    /**
+     * The URL of Privacy Policy Page.
+     */
+    privacyPolicyURL: 'https://digitalesozialearbeit.de/kontakt/datenschutzerklaerung',
+
+    /**
+     * The URL of Terms and Conditions Page.
+     */
+    termsAndConditionsURL: 'https://digitalesozialearbeit.de/kontakt/datenschutzerklaerung'
+};
---
+++ app/features/welcome/styled/Wrapper.js
@@ -0,0 +1,11 @@
+// @flow
+
+import styled from 'styled-components';
+
+export default styled.div`
+    background: url('file:///C:/Users/saini/Downloads/Compressed/jitsi-meet-electron-master/jitsi-meet-electron-master/assets/digital.jpg');
+    background-size:cover;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
